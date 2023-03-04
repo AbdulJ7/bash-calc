868b3afc6d79ded53c9f426b5ba3a9827245b94e
Abdul Jaber <121806946+AbdulJ7@users.noreply.github.com>
2023-03-04 13:03:26 +0000

    Create calculator.sh
    added addition in this script

---
+++ calculator.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+function main() {
+   echo -e "Enter 1st Number:\n"
+   read n1
+   echo -e "Enter 2nd Number:\n"
+   read n2
+   add=$(($n1+$n2))
+   echo -e "Addition is $add"  
+}
+
+main()
+ 
