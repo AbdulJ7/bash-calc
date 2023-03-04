4550f95fa396751f8873ec0c477dea6b924f7df2
Abdul Jaber <121806946+AbdulJ7@users.noreply.github.com>
2023-03-04 13:47:11 +0000

    Update calculator.sh
    pakka update

--- calculator.sh
+++ calculator.sh
@@ -1,26 +1,40 @@
 #!/usr/bin/bash
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-SCRIPTNAME="$0"
-ARGS="$@"
-BRANCH="main"
-
 self_update() {
-    cd $SCRIPTPATH
-    git fetch
+# store the current dir
+CUR_DIR=$(pwd)
 
-    [ -n $(git diff --name-only origin/$BRANCH | grep $SCRIPTNAME) ] && {
-        echo "Found a new version of me, updating myself..."
-        git pull --force
-        git checkout $BRANCH
-        git pull --force
-        echo "Running the new version..."
-        exec "$SCRIPTNAME" "$@"
+# Let the person running the script know what's going on.
+echo -e "\n\033[1mPulling in latest changes for all repositories...\033[0m\n"
 
-        # Now exit this old instance
-        exit 1
-    }
-    echo "Already the latest version."
+# Find all git repositories and update it to the master latest revision
+for i in $(find -L . -name ".git" | cut -c 3-); do
+    echo "";
+    echo -e "\033[33m"+$i+"\033[0m";
+
+    # We have to go to the .git parent directory to call the pull command
+    cd "$i";
+    cd ..;
+
+    # git checkout master;
+    git remote prune origin;
+    git fetch origin;
+    git pull;
+
+    if [ -f "yarn.lock" ]
+    then
+        yarn install --prefer-offline;
+    fi
+
+    if [ -f "package-lock.json" ]
+    then
+        npm ci --prefer-offline --no-audit;
+    fi  
+
+    # lets get back to the CUR_DIR
+    cd $CUR_DIR
+done
+
+echo -e "\n\033[32mComplete!\033[0m\n"
 }
 
 main() {
