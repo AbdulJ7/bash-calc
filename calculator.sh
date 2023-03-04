1285076d8b5475c4380776ba5c9ddcf0057f45dc
Abdul Jaber <121806946+AbdulJ7@users.noreply.github.com>
2023-03-04 13:32:55 +0000

    Update calculator.sh
    again self update script

--- calculator.sh
+++ calculator.sh
@@ -9,4 +9,33 @@ function main() {
    echo -e "Addition is $add"  
 }
 
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPTNAME="$0"
+ARGS="$@"
+BRANCH="main"
+
+self_update() {
+    cd $SCRIPTPATH
+    git fetch
+
+    [ -n $(git diff --name-only origin/$BRANCH | grep $SCRIPTNAME) ] && {
+        echo "Found a new version of me, updating myself..."
+        git pull --force
+        git checkout $BRANCH
+        git pull --force
+        echo "Running the new version..."
+        exec "$SCRIPTNAME" "$@"
+
+        # Now exit this old instance
+        exit 1
+    }
+    echo "Already the latest version."
+}
+
+main() {
+   echo "Running"
+}
+
+self_update
 main
