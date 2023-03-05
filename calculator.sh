b8f664c5b323069957e8a881463fb2de5a666de6
Abdul Jaber <121806946+AbdulJ7@users.noreply.github.com>
2023-03-05 05:28:38 +0000

    Update calculator.sh
    updated the update function

--- calculator.sh
+++ calculator.sh
@@ -4,22 +4,13 @@ self_update() {
 CUR_DIR=~/.local/share/bash-calc
 
 # Let the person running the script know what's going on.
-echo -e "\n\033[1mPulling in latest changes for all repositories...\033[0m\n"
+echo -e "\n\033[1mPulling in latest changes...\033[0m\n"
 
-# Find all git repositories and update it to the master latest revision
-for i in $(find -L . -name ".git" | cut -c 3-); do
-    echo "";
-    echo -e "\033[33m"+$i+"\033[0m";
-
-    # We have to go to the .git parent directory to call the pull command
-    cd "$i";
-    cd ..;
-
+cd $CUR_DIR
     # git checkout master;
     git remote prune origin;
     git fetch origin;
     git pull;
-done
 
 echo -e "\n\033[32mComplete!\033[0m\n"
 }
