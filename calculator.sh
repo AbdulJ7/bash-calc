3a95e57a3865e85ac7e8637e8e00870388048ccd
Abdul Jaber <121806946+AbdulJ7@users.noreply.github.com>
2023-03-05 06:23:01 +0000

    Update calculator.sh
    fixed errors in update

--- calculator.sh
+++ calculator.sh
@@ -4,7 +4,7 @@ self_update() {
 # Uncomment if you want the script to always use the scripts
 # directory as the folder to look through
 #REPOSITORIES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-REPOSITORIES='~/.local/share/bash-calc'
+REPOSITORIES=~/.local/share/bash-calc
 
 IFS=$'\n'
 
