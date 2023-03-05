cfa5b607b2bdc101fb5cdeec4d3c2fc153f501cb
Abdul Jaber <121806946+AbdulJ7@users.noreply.github.com>
2023-03-05 07:11:45 +0000

    Update calculator.sh
    e

--- calculator.sh
+++ calculator.sh
@@ -1,33 +1,6 @@
 #!/usr/bin/bash
 self_update() {
-  CWD=$HOME/.local/share/bash-calc
-
-for d in `find $CWD -name ".git" -type d -maxdepth 2`;
-do
-    REPO_FOUND=true
-    cd $d/..
-    PROJECT_DIR=`pwd`
-    REMOTE_NAME=`git remote`
-    DEFAULT_BRANCH=`git remote show ${REMOTE_NAME} | sed -n '/HEAD branch/s/.*: //p'`
-    CURRENT_BRANCH=`git branch --show-current`
-    if [ "$DEFAULT_BRANCH" == "$CURRENT_BRANCH" ]; then
-      echo "Attempting to sync pull ${PROJECT_DIR}"
-      git pull
-    else
-      if [ -z "$DEFAULT_BRANCH" ]
-      then
-        echo "Unable to fetch ${PROJECT_DIR}. Cannot determine default branch"
-        # too lazy to test, maybe we could try main or master and just handle the error
-      else
-        echo "Attempting to sync fetch ${PROJECT_DIR}"
-        git fetch $REMOTE_NAME $DEFAULT_BRANCH:$DEFAULT_BRANCH
-      fi
-    fi
-done
-
-if [ "true" != "$REPO_FOUND" ]; then
-  echo "No repo in current directory. Nothing updated"
-fi
+  git pull -f
 }
 
 main() {
